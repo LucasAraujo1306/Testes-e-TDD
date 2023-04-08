4846db97f7b2ad2c4cccab43b5aebc869865973b
LucasAraujo1306 <lucasisaac007@gmail.com>
2023-04-07 23:39:12 -0300

    criado a parte dos teste

---
+++ src/libs/math.test.ts
@@ -0,0 +1,30 @@
+import { Math } from './Math'
+
+describe('Testing Math libary', () => {
+    it('should sum two numbers correctly', () => {
+        const response = Math.sum(5, 10);
+        expect(response).toBe(15);
+    })
+
+    it('should subtract two number correctly', () => {
+        const response = Math.sub(4, 2);
+        expect(response).toBe(2);
+    })
+
+    it('should divide two number correctly', () => {
+        const response = Math.div(10, 2);
+        expect(response).toBe(5);
+
+        const response2 = Math.div(10, 0);
+        expect(response2).toBe(false);
+    })
+
+    it('should multiply two number correctly', () => {
+        const response = Math.mut(5, 2);
+        expect(response).toBe(10);
+
+        const response2 = Math.mut(0, 2);
+        expect(response2).toBe(0);
+    })
+})
+
